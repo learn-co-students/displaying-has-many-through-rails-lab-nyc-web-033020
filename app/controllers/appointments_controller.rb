37dd54096974a9bdbc86bcc78abadef79867100d
Garen Sahagian <g.s.sahagian@gmail.com>
2020-04-30 12:36:13 -0400

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,36 @@
+class AppointmentsController < ApplicationController
+    
+    def index
+        @appointments = Appointment.all
+    end
+
+    def show
+        @appointment = Appointment.find(params[:id])
+    end
+
+    def new
+        
+    end
+
+    def create
+        
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    def appointment_params
+
+    end
+end
---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,35 @@
+class DoctorsController < ApplicationController
+    def index
+        
+    end
+
+    def show
+        @doctor = Doctor.find(params[:id])
+    end
+
+    def new
+        
+    end
+
+    def create
+        
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    def doctor_params
+
+    end
+end
